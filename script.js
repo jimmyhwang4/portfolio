e8b0ec3112c53eeab69d69157d3514e632a44e08
jimmyhwang4 <35846105+jimmyhwang4@users.noreply.github.com>
2020-03-25 16:35:47 -0700

    Portfolio

---
+++ script.js
@@ -0,0 +1,15 @@
+var i = 0;
+var txt = 'IniTiaLIzIng pOrTFoLIo....'; /* The text */
+var speed = 100; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+
+  if (i == txt.length) {
+    window.location.href = "main.html"
+  }
+}
